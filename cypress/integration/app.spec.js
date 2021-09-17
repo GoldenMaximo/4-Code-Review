a1ce406d553a94e6db028b2d03056646616904b9
Gustavo Máximo <gfmaximo97@gmail.com>
2021-09-17 14:33:47 -0300

    test: Add E2E tests.

---
+++ cypress/integration/app.spec.js
@@ -0,0 +1,69 @@
+/// <reference types="cypress" />
+
+context('E2E', () => {
+    describe('Single pass through the entire app', () => {
+        it('should search for good stuff', () => {
+            cy.intercept({
+                method: 'GET',
+                url: `${Cypress.env('githubSearchEndpoint')}*`,
+            }).as('githubSearchEndpoint');
+
+            cy.intercept({
+                method: 'GET',
+                url: `${Cypress.env('nasaPlanetaryEndpoint')}*`,
+            }).as('nasaPlanetaryEndpoint');
+
+            cy.visit(`http://localhost:${Cypress.env('port')}/`);
+
+            cy.get('input').type('node').should('have.value', 'node');
+
+            cy.get('button').click();
+
+            cy.url().should('include', '/search?repository-name=node');
+
+            cy.get('.ant-page-header-heading-title').contains('node');
+
+            cy.wait('@githubSearchEndpoint').then(interception => {
+                assert.isNotNull(interception.response.body, '1st API call has data');
+
+                cy.get('.ant-list li').first().get('a').contains('node');
+            });
+
+            cy.wait('@nasaPlanetaryEndpoint').then(interception => {
+                assert.isNotNull(interception.response.body, '2nd API call has data');
+
+                expect(cy.get('.ant-list li').first('img')).to.exist;
+            });
+
+            cy.get('.ant-pagination li').eq(2).click();
+
+            cy.wait('@githubSearchEndpoint').then(interception => {
+                assert.isNotNull(interception.response.body, '3rd API call has data');
+
+                cy.get('.ant-list li').first().get('a').contains('node');
+            });
+
+            cy.wait('@nasaPlanetaryEndpoint').then(interception => {
+                assert.isNotNull(interception.response.body, '4th API call has data');
+
+                expect(cy.get('.ant-list li').first('img')).to.exist;
+            });
+
+            cy.get('input.ant-input').type('react').should('have.value', 'react').type('{enter}');
+
+            cy.url().should('include', '/search?repository-name=react');
+
+            cy.get('.ant-page-header-heading-title').contains('react');
+
+            cy.wait('@githubSearchEndpoint').then(interception => {
+                assert.isNotNull(interception.response.body, '5th API call has data');
+
+                cy.get('.ant-list li').first().get('a').contains('react');
+            });
+
+            cy.get('[aria-label="home"]').parent().click();
+
+            cy.url().should('not.include', '/search');
+        });
+    });
+});
